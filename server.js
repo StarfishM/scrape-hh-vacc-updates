bf5272d92ca2b0af9c5bcc01e7f9cf129d2cee13
Merle Fischer <merle_canada@hotmail.com>
2021-04-22 20:52:38 +0200

    adds start for checking if it's turn of group 65+, still need to finetune and change that logic

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const PORT = 8080;
 const compareState = require("./compareState.json");
 const recentState = require("./recentState.json");
 
+app.get("/", async (req, res) => {
+    await comparePastAndCurrPageState();
+    return res.send(`<h1> well hello there 👋 </h1>
+    <h2>hh vacc page update:${recentState[0].dateUpdated}</h2>
+    `);
+});
+
 const getCurrentPageState = async () => {
     try {
         const { data } = await axios.get(
@@ -14,17 +21,21 @@ const getCurrentPageState = async () => {
         );
         const $ = cheerio.load(data);
         const paragraphs = [];
-
-        $("div.richtext p").map((_idx, el) => {
+        // might not need this maß, might only need the date!!!!
+        $("div.richtext p").map((i, el) => {
             const paraText = $(el).text();
-            if (_idx === 0) {
-                console.log("first paragraph!");
+            if (i === 0) {
                 const matchDate = /(\d\d)\.(\d\d)\.(\d\d\d\d)/;
                 const dateUpdated = matchDate.exec(paraText);
                 paragraphs.push({ dateUpdated: dateUpdated[0] });
             }
 
-            paragraphs.push({ [_idx]: paraText });
+            paragraphs.push({ [i]: paraText });
+        });
+        // definitely keep those for checking if it's mums turn yet!
+        $("ul.normal-list li").map((i, el) => {
+            const lis = $(el).text();
+            paragraphs.push({ group: lis });
         });
         return paragraphs;
     } catch (err) {
@@ -41,10 +52,8 @@ const writeRecentState = (arr) => {
 
 const pageUpdated = (refArr, currArr) => {
     if (refArr[0].dateUpdated === currArr[0].dateUpdated) {
-        console.log("update date the same");
         return false;
     } else {
-        console.log("updated date not the same");
         return true;
     }
 };
@@ -55,15 +64,32 @@ const comparePastAndCurrPageState = async () => {
     );
     writeRecentState(paragraphs);
     const updated = pageUpdated(compareState, recentState);
+    const yes = checkIfTurn(recentState);
     console.log("updated?", updated);
+    console.log("yes?", yes);
     if (updated) {
         // send email! OR text message
         // make recentState new compareState
+    } else if (yes) {
+        // mum's turn
     } else {
         // don't do anything
     }
 };
 
-comparePastAndCurrPageState();
+const checkIfTurn = (arr) => {
+    let oui = false;
+    arr.map((el) => {
+        // instead get number value from paragrpahk, and check if it is <= 65 , if yes, set to true!
+        if (
+            el.group &&
+            (el.group.indexOf("65") > -1 || el.group.indexOf("60") > -1)
+        ) {
+            console.log("65 is part of the group");
+            oui = true;
+        }
+    });
+    return oui;
+};
 
 app.listen(PORT, console.log(`server running on ${PORT}`));
